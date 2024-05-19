20817d27ce348ef70cf62e77499996c02748f24b
ynnck <o5YV;)YsA4,L6#D>
2024-05-19 15:23:15 +0800

    Refactor

--- src/PolizeiBot.ts
+++ src/PolizeiBot.ts
@@ -6,38 +6,33 @@ import logger from './config/logger';
  * The bot P0lizeiBot needs to be added to the group and promoted to administrator
  */
 class PolizeiBot {
-    private readonly blacklistedTerms = ["lah", "lor"];
 
-    constructor(private telegramBot: TelegramBot) {
-    }
+    constructor(private telegramBot: TelegramBot) {}
 
-    initialize() {
-        this.telegramBot.onText(/\/blacklist/, async (message) => {
-            const chatId = message.chat.id;
-            await this.sendMessage(chatId, `🚫Blacklisted terms: ${this.blacklistedTerms.join(", ")}`);
-        });
-
+    onMessage(callback: (message: TelegramBot.Message) => void) {
         this.telegramBot.on('message', async (message) => {
-            if (message.text && this.containsBlacklistedTerm(message)) {
-                try {
-                    await this.telegramBot.deleteMessage(message.chat.id, message.message_id);
-                    logger.info(`Deleted message containing blacklisted term. Chat id: ${message.chat.id}, message id: ${message.message_id}. Message: ${message.text}, user: ${message.from.first_name} ${message.from.last_name}`);
-                } catch (e) {
-                    logger.error(`Failed to delete message (chat id: ${message.chat.id}, message id: ${message.message_id}): ${e}`);
-                }
-            }
+            callback(message);
         });
     }
 
-    private containsBlacklistedTerm(message: TelegramBot.Message) {
-        return this.blacklistedTerms.some((v) => message.text.toLowerCase().includes(v.toLowerCase()));
+    onGetBlacklist(callback: (message: TelegramBot.Message) => void) {
+        this.telegramBot.onText(/\/blacklist/, async (message) => callback(message));
     }
 
-    private async sendMessage(chatId: number, message: string) {
+    async sendMessage(chatId: number, message: string) {
         try {
             await this.telegramBot.sendMessage(chatId, message);
         } catch (e) {
             logger.error(`Failed to send message: ${e}`);
+        }
+    }
+
+    async deleteMessage(message: TelegramBot.Message) {
+        try {
+            await this.telegramBot.deleteMessage(message.chat.id, message.message_id);
+            logger.info(`Deleted message containing blacklisted term. Chat id: ${message.chat.id}, message id: ${message.message_id}. ${message.from.first_name} ${message.from.last_name}: ${message.text}`);
+        } catch (e) {
+            logger.error(`Failed to delete message (chat id: ${message.chat.id}, message id: ${message.message_id}): ${e}`);
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import logger from './config/logger';
 
 require('dotenv').config();
 
+const BLACKLIST = ["lah", "lor"];
+
 const bot = new PolizeiBot(new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true}));
-bot.initialize();
+
+bot.onGetBlacklist(async (message) => {
+    await bot.sendMessage(message.chat.id, `🚫Blacklisted terms: ${BLACKLIST.join(", ")}`);
+});
+
+bot.onMessage(async (message) => {
+    if (message.text && containsBlacklistedTerm(message.text)) {
+        await bot.deleteMessage(message);
+    }
+});
+
+function containsBlacklistedTerm(messageText: string) {
+    return BLACKLIST.some((v) => messageText.toLowerCase().includes(v.toLowerCase()));
+}
 
 logger.info('Application started. Bot initialized and running');
